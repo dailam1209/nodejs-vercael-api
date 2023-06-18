6a9bbb935c15ae7e9839336458cafe1a27970fe0
dailam1209 <lamngocdai050920@gmail.com>
2023-06-18 15:09:19 +0700

    cros-3

--- index.js
+++ index.js
@@ -32,18 +32,23 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true,limit:"50mb"}));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors({
-    origin: "http://localhost:3001/",
-    origin: "*",
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
-  }));
+// app.use(cors({
+//     origin: "http://localhost:3001/",
+//     origin: "*",
+//     methods: "GET,POST,PUT,DELETE",
+//     credentials: true,
+//   }));
 
 app.use((req, res, next) => {
+  // 👇️ specify CORS headers to send 👇️
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    'Access-Control-Allow-Methods',
+    'POST, PUT, PATCH, GET, DELETE, OPTIONS',
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization',
   );
   next();
 });
