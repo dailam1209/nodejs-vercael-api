91887ca1f8b896d590aa7c8b476537fc2a6c01bf
dailam1209 <lamngocdai050920@gmail.com>
2023-06-18 15:29:21 +0700

    cros-5

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true,limit:"50mb"}));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors({
-  origin: 'https://vercel-nodejs.onrender.com', // use your actual domain name (or localhost), using * is not recommended
+  origin: 'http://localhost:3000', // use your actual domain name (or localhost), using * is not recommended
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
   credentials: true
