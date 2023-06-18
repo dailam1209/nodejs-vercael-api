b438335da2a73a5612efeb5edabccc66790fe2e1
dailam1209 <lamngocdai050920@gmail.com>
2023-06-18 21:06:45 +0700

    cros-host-1

--- index.js
+++ index.js
@@ -57,20 +57,11 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 const whitelist = ['http://localhost:3001', 'https://vercel-nodejs.onrender.com'];
 
 // ✅ Enable pre-flight requests
-app.options('*', cors());
-
-const corsOptions = {
-  credentials: true,
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
-
-app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 app.use(passport.initialize());
