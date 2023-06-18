18610e4d77d34104f4b950d8e28b34b734777c08
dailam1209 <lamngocdai050920@gmail.com>
2023-06-18 15:27:29 +0700

    cros-4

--- index.js
+++ index.js
@@ -32,26 +32,26 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true,limit:"50mb"}));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-// app.use(cors({
-//     origin: "http://localhost:3001/",
-//     origin: "*",
-//     methods: "GET,POST,PUT,DELETE",
-//     credentials: true,
-//   }));
+app.use(cors({
+  origin: 'https://vercel-nodejs.onrender.com', // use your actual domain name (or localhost), using * is not recommended
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+  credentials: true
+  }));
 
-app.use((req, res, next) => {
-  // 👇️ specify CORS headers to send 👇️
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    'Access-Control-Allow-Methods',
-    'POST, PUT, PATCH, GET, DELETE, OPTIONS',
-  );
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization',
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   // 👇️ specify CORS headers to send 👇️
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     'Access-Control-Allow-Methods',
+//     'POST, PUT, PATCH, GET, DELETE, OPTIONS',
+//   );
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization',
+//   );
+//   next();
+// });
 
 
 app.use(passport.initialize());
