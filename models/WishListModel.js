1622af58bffcb6418f3b03f366b20abc5f8187eb
dailam1209 <lamngocdai050920@gmail.com>
2023-06-19 17:32:46 +0700

    wish-list-3-router

--- models/WishListModel.js
+++ models/WishListModel.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const WishListSchema = mongoose.Schema({
-    userId: {type: mongoose.Schema.Types.ObjectId, required: [true, "Please enter your user id"]},
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: "customer", required:  [true, "Please enter your user id"]},
     code: {type: String, required: [true, "Please enter product code"]},
-}
+    },
+    { timestamps: true }
 );
 
 module.exports = mongoose.model("WishList", WishListSchema);
