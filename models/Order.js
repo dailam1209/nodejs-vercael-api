7068bcab5b3a7df1b6a355e81656af00e01eee36
dailam1209 <lamngocdai050920@gmail.com>
2023-07-06 23:20:54 +0700

    try-catch

--- models/Order.js
+++ models/Order.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const OrderSchema = mongoose.Schema({
-        userId: {type: String, required: true },
+        userId: {type: mongoose.Schema.Types.ObjectId, ref: "order", required: true },
         amount: { type: String, required: true},
         payment: { type: String, required: true },
         status: { type: String, default: "pending"}
