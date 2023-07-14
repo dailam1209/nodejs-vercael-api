68a91116a3c05763a59dd5029fe4d45903f4660c
dailam1209 <lamngocdai050920@gmail.com>
2023-07-14 09:24:00 +0700

    change-url-4

--- models/Product.js
+++ models/Product.js
@@ -23,7 +23,9 @@ const ProductSchema = new mongoose.Schema({
     description: { type: String, required: true },
     price: { type: Number, required: true },
     stock: { type: Number, required: true},
-    code: { type: String},
+    discount: { type: Number, required: true},
+    code: { type: String, required: true},
+    describe: { type: String, required: true},
     brand: { type: String, required: true},
     category: {type: String, required: true},
     createAt:{
